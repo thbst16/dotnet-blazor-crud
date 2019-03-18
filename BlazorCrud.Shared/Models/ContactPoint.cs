bdec7b8c0ffca54474b3db58252e303948195b6e
Thomas Beck <thbst16@hotmail.com>
2019-03-17 21:10:59 -0400

    Get project current and prepare for deployment.

---
+++ BlazorCrud.Shared/Models/ContactPoint.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorCrud.Shared.Models
+{
+    public class ContactPoint
+    {
+        public string Type { get; set; }
+        public string Number { get; set; }
+        public string Use { get; set; }
+    }
+}
