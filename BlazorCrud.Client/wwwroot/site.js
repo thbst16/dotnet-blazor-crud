610bff01fe69b8e4bdedfc8048ac2baa14194157
Thomas Beck <thbst16@hotmail.com>
2019-05-05 01:56:36 -0400

    Added support for base binary file upload

---
+++ BlazorCrud.Client/wwwroot/site.js
@@ -0,0 +1,17 @@
+﻿window.readFileAsBase64 = (fileInput) => {
+    const readAsDataURL = (fileInput) => {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.onerror = () => {
+                reader.abort();
+                reject(new Error("Error reading file."));
+            };
+            reader.addEventListener("load", () => {
+                resolve(reader.result);
+            }, false);
+            reader.readAsDataURL(fileInput.files[0]);
+        });
+    };
+
+    return readAsDataURL(fileInput);
+};
---
+++ BlazorCrud.Server/Controllers/UploadController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace BlazorCrud.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        /// <summary>
+        /// Creates a file.
+        /// </summary>
+        [HttpPost]
+        public async Task<IActionResult> Create()
+        {
+            var tempFileName = Path.GetTempFileName();
+            using (var writer = System.IO.File.OpenWrite(tempFileName))
+            {
+                await Request.Body.CopyToAsync(writer);
+            }
+            return Ok(Path.GetFileNameWithoutExtension(tempFileName));
+        }
+    }
+}
