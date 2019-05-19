d33dc27e23359663de7804dd9bc0b43cb005222e
Thomas Beck <thomas@beckshome.com>
2019-05-19 15:15:05 -0400

    Increased Blazor timeout in test to 10 mins and removed Google search code.

--- BlazorCrud.Tests.E2E/UITest.cs
+++ BlazorCrud.Tests.E2E/UITest.cs
@@ -25,25 +25,18 @@ namespace BlazorCrud.Tests.E2E
             else
             {
                 driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), chromeOptions);
-            }
-                        
+            }         
             using (driver)
             {
-                //driver.Navigate().GoToUrl("https://becksblazor.azurewebsites.net/index.html");
-                //var waitLoad = new WebDriverWait(driver, TimeSpan.FromSeconds(240));
-                //waitLoad.Until(d => d.FindElement(By.PartialLinkText("Login")));
-                //IWebElement LoginLink = driver.FindElement(By.PartialLinkText("Login"));
-                //LoginLink.Click();
-                //var waitClick = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
-                //waitClick.Until(d => d.Url.Equals("https://becksblazor.azurewebsites.net/user/login"));
-                //Assert.AreEqual(driver.Url, "https://becksblazor.azurewebsites.net/user/login");
-                driver.Navigate().GoToUrl("http://www.google.com/");
-                IWebElement query = driver.FindElement(By.Name("q"));
-                query.SendKeys("Blazor");
-                query.Submit();
-                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
-                wait.Until(d => d.Title.StartsWith("Blazor", StringComparison.OrdinalIgnoreCase));
-                Assert.AreEqual(driver.Title, "Blazor - Google Search");
+                driver.Manage().Window.FullScreen();
+                driver.Navigate().GoToUrl("https://becksblazor.azurewebsites.net/index.html");
+                var waitLoad = new WebDriverWait(driver, TimeSpan.FromMinutes(10));
+                waitLoad.Until(d => d.FindElement(By.PartialLinkText("Login")));
+                IWebElement LoginLink = driver.FindElement(By.PartialLinkText("Login"));
+                LoginLink.Click();
+                var waitClick = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                waitClick.Until(d => d.Url.Equals("https://becksblazor.azurewebsites.net/user/login"));
+                Assert.AreEqual(driver.Url, "https://becksblazor.azurewebsites.net/user/login");
             }
         }
     }
