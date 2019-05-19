aa4a4dd23eaa4cf44e3ad3e6ae8c6b832f568dce
Thomas Beck <thomas@beckshome.com>
2019-05-19 14:58:57 -0400

    Attempt UI tests without Blazor

--- BlazorCrud.Tests.E2E/UITest.cs
+++ BlazorCrud.Tests.E2E/UITest.cs
@@ -29,14 +29,21 @@ namespace BlazorCrud.Tests.E2E
                         
             using (driver)
             {
-                driver.Navigate().GoToUrl("https://becksblazor.azurewebsites.net/index.html");
-                var waitLoad = new WebDriverWait(driver, TimeSpan.FromSeconds(240));
-                waitLoad.Until(d => d.FindElement(By.PartialLinkText("Login")));
-                IWebElement LoginLink = driver.FindElement(By.PartialLinkText("Login"));
-                LoginLink.Click();
-                var waitClick = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
-                waitClick.Until(d => d.Url.Equals("https://becksblazor.azurewebsites.net/user/login"));
-                Assert.AreEqual(driver.Url, "https://becksblazor.azurewebsites.net/user/login");
+                //driver.Navigate().GoToUrl("https://becksblazor.azurewebsites.net/index.html");
+                //var waitLoad = new WebDriverWait(driver, TimeSpan.FromSeconds(240));
+                //waitLoad.Until(d => d.FindElement(By.PartialLinkText("Login")));
+                //IWebElement LoginLink = driver.FindElement(By.PartialLinkText("Login"));
+                //LoginLink.Click();
+                //var waitClick = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
+                //waitClick.Until(d => d.Url.Equals("https://becksblazor.azurewebsites.net/user/login"));
+                //Assert.AreEqual(driver.Url, "https://becksblazor.azurewebsites.net/user/login");
+                driver.Navigate().GoToUrl("http://www.google.com/");
+                IWebElement query = driver.FindElement(By.Name("q"));
+                query.SendKeys("Blazor");
+                query.Submit();
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+                wait.Until(d => d.Title.StartsWith("Blazor", StringComparison.OrdinalIgnoreCase));
+                Assert.AreEqual(driver.Title, "Blazor - Google Search");
             }
         }
     }
