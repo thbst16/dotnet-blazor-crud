7825fce634291031c6153064143b95fd398f2ff4
Thomas Beck <thomas@beckshome.com>
2019-05-19 16:18:17 -0400

    Refactored UI Test and Added Second Test Method

--- BlazorCrud.Tests.E2E/UITest.cs
+++ BlazorCrud.Tests.E2E/UITest.cs
@@ -11,13 +11,30 @@ namespace BlazorCrud.Tests.E2E
     [TestClass]
     public class UITest
     {
-        [TestMethod]
-        public void ClickLoginButtonRoutesToLoginPage()
+        private IWebDriver driver;
+        private TestContext testContextInstance;
+
+        public UITest() { }
+
+        // Gets or sets the test context, which provides information about the current test run
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        [TestInitialize()]
+        public void SetupTest()
         {
             var chromeOptions = new ChromeOptions();
             // chromeOptions.AddArgument("--headless");
 
-            ChromeDriver driver;
             if (Environment.GetEnvironmentVariable("ChromeWebDriver") != null)
             {
                 driver = new ChromeDriver(Environment.GetEnvironmentVariable("ChromeWebDriver"), chromeOptions);
@@ -25,10 +42,15 @@ namespace BlazorCrud.Tests.E2E
             else
             {
                 driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), chromeOptions);
-            }         
+            }
+            driver.Manage().Window.FullScreen();
+        }
+
+        [TestMethod()]
+        public void ClickLoginButtonRoutesToLoginPage()
+        {             
             using (driver)
             {
-                driver.Manage().Window.FullScreen();
                 driver.Navigate().GoToUrl("https://becksblazor.azurewebsites.net/index.html");
                 var waitLoad = new WebDriverWait(driver, TimeSpan.FromMinutes(10));
                 waitLoad.Until(d => d.FindElement(By.PartialLinkText("Login")));
@@ -38,6 +60,28 @@ namespace BlazorCrud.Tests.E2E
                 waitClick.Until(d => d.Url.Equals("https://becksblazor.azurewebsites.net/user/login"));
                 Assert.AreEqual(driver.Url, "https://becksblazor.azurewebsites.net/user/login");
             }
+        }
+
+        [TestMethod()]
+        public void ClickDashboardButtonRoutesToDashboardPage()
+        {
+            using (driver)
+            {
+                driver.Navigate().GoToUrl("https://becksblazor.azurewebsites.net/index.html");
+                var waitLoad = new WebDriverWait(driver, TimeSpan.FromMinutes(10));
+                waitLoad.Until(d => d.FindElement(By.PartialLinkText("Dashboard")));
+                IWebElement DashboardLink = driver.FindElement(By.PartialLinkText("Dashboard"));
+                DashboardLink.Click();
+                var waitClick = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                waitClick.Until(d => d.Url.Equals("https://becksblazor.azurewebsites.net/dashboard"));
+                Assert.AreEqual(driver.Url, "https://becksblazor.azurewebsites.net/dashboard");
+            }
+        }
+
+        [TestCleanup()]
+        public void CleanupTest()
+        {
+            driver.Quit();
         }
     }
 }
