bafc3b8169fa76a1dfe6404fdf70be1d06eb82e1
Thomas Beck <thomas@beckshome.com>
2020-01-25 18:49:36 -0500

    Updated processing job to process well-formatted patient, claims, and organization JSON files.

--- BlazorCrud.Server/Services/ProcessFileJob.cs
+++ BlazorCrud.Server/Services/ProcessFileJob.cs
@@ -7,6 +7,7 @@ using Quartz;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace BlazorCrud.Server.Services
@@ -15,7 +16,10 @@ namespace BlazorCrud.Server.Services
     public class ProcessFileJob : IJob
     {
         private readonly IServiceProvider _provider;
-        private UploadContext _context;
+        private PatientContext _patientContext;
+        private OrganizationContext _organizationContext;
+        private ClaimContext _claimContext;
+        private UploadContext _uploadContext;
         private readonly ILogger<ProcessFileJob> _logger;
 
         public ProcessFileJob(IServiceProvider provider, IConfiguration config, ILogger<ProcessFileJob> logger)
@@ -28,7 +32,10 @@ namespace BlazorCrud.Server.Services
         {
             using (var scope = _provider.CreateScope())
             {
-                _context = scope.ServiceProvider.GetService<UploadContext>();
+                _uploadContext = scope.ServiceProvider.GetService<UploadContext>();
+                _patientContext = scope.ServiceProvider.GetService<PatientContext>();
+                _organizationContext = scope.ServiceProvider.GetService<OrganizationContext>();
+                _claimContext = scope.ServiceProvider.GetService<ClaimContext>();
                 _logger.LogInformation("File Processing Job Initiated: " + DateTime.Now.ToString("dddd, MMMM dd, yyyy HH:mm:ss.fffK"));
                 List<Upload> unprocessedUploads = GetUnprocessedUploads();
                 _logger.LogInformation("Count of files requiring processing: " + unprocessedUploads.Count);
@@ -43,7 +50,7 @@ namespace BlazorCrud.Server.Services
 
         private List<Upload> GetUnprocessedUploads()
         {
-            List<Upload> unprocessedUploads = (from u in _context.Uploads
+            List<Upload> unprocessedUploads = (from u in _uploadContext.Uploads
                                                    where u.ProcessedTimestamp.HasValue != true
                                                    select u).ToList();
             return unprocessedUploads;
@@ -53,10 +60,57 @@ namespace BlazorCrud.Server.Services
         {
             foreach (Upload u in uploads)
             {
+                // Base64 decode file
+                byte[] base64Data = System.Convert.FromBase64String(u.FileContent);
+                string base64Decoded = System.Text.ASCIIEncoding.UTF8.GetString(base64Data);
+
+                try
+                {
+                    // Deserialize and process JSON file, persisting entities to database
+                    if (u.FileName.ToLower() == "patients")
+                    {
+                        List<Patient> patients = JsonSerializer.Deserialize<List<Patient>>(base64Decoded);
+                        foreach (Patient p in patients)
+                        {
+                            p.ModifiedDate = DateTime.Now;
+                            _patientContext.Patients.Add(p);
+                        }
+                        _patientContext.SaveChanges();
+                    }
+
+                    if (u.FileName.ToLower() == "organizations")
+                    {
+                        List<Organization> organizations = JsonSerializer.Deserialize<List<Organization>>(base64Decoded);
+                        foreach (Organization o in organizations)
+                        {
+                            o.ModifiedDate = DateTime.Now;
+                            _organizationContext.Organizations.Add(o);
+                        }
+                        _organizationContext.SaveChanges();
+                    }
+
+                    if (u.FileName.ToLower() == "claims")
+                    {
+                        List<Claim> claims = JsonSerializer.Deserialize<List<Claim>>(base64Decoded);
+                        foreach (Claim c in claims)
+                        {
+                            c.ModifiedDate = DateTime.Now;
+                            _claimContext.Claims.Add(c);
+                        }
+                        _claimContext.SaveChanges();
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    _logger.LogInformation("Exception Type: " + ex.GetType());
+                }
+
+                // Update file processing status in the system
                 u.ProcessedTimestamp = DateTime.Now;
                 _logger.LogInformation("File Id: " + u.Id + " Name: " + u.FileName + " processed at " + DateTime.Now.ToString("dddd, MMMM dd, yyyy HH:mm:ss.fffK"));
             }
-            _context.SaveChanges();
+            _uploadContext.SaveChanges();
         }
     }
 }
