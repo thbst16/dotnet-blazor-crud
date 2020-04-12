c53045e59516e2a8cce6ba39c8f791903a9604ef
Thomas Beck <thomas@beckshome.com>
2020-04-11 21:34:54 -0400

    Updated tpye ahead to expand claims model and include fluent validation

---
+++ BlazorCrud.Client/Shared/ClaimModelView.cs
@@ -0,0 +1,40 @@
+﻿namespace BlazorCrud.Client.Shared
+{
+    public class ClaimModelView
+    {
+        public _Patient SelectedPatient { get; set; }
+        public _Provider SelectedProvider { get; set; }
+        public string Type { get; set; }
+        public string Status { get; set; }
+    }
+
+    public class _Patient
+    {
+        public _Patient() { }
+        public _Patient(int id, string name, string location)
+        {
+            Id = id;
+            Name = name;
+            Location = location;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+    }
+
+    public class _Provider
+    {
+        public _Provider() { }
+        public _Provider(int id, string name, string location)
+        {
+            Id = id;
+            Name = name;
+            Location = location;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+    }
+}
---
+++ BlazorCrud.Client/Shared/ClaimModelViewValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace BlazorCrud.Client.Shared
+{
+    public class ClaimModelViewValidator : AbstractValidator<ClaimModelView>
+    {
+        public ClaimModelViewValidator()
+        {
+            CascadeMode = CascadeMode.StopOnFirstFailure;
+
+            RuleFor(claim => claim.SelectedPatient).NotEmpty().WithMessage("Patient is a required field.");
+            RuleFor(claim => claim.SelectedProvider).NotEmpty().WithMessage("Provider is a required field.");
+            RuleFor(claim => claim.Status).NotEmpty().WithMessage("Status is a required field.");
+            RuleFor(claim => claim.Type).NotEmpty().WithMessage("Type is a required field.");
+        }
+    }
+}
