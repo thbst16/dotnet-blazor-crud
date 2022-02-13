baf7ee1d9801213f1d900632feea88d9f4504578
Thomas Beck <thomas@beckshome.com>
2022-02-13 19:33:51 +0000

    removed swagger from debug settings

--- Blazorcrud.Server/Program.cs
+++ Blazorcrud.Server/Program.cs
@@ -66,12 +66,6 @@ using (var scope = app.Services.CreateScope())
 if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
-    app.UseSwagger();
-    app.UseSwaggerUI(c => 
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "blazorcrud.api v1");
-        c.DefaultModelsExpandDepth(-1);
-    });
 }
 else
 {
@@ -79,6 +73,13 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI(c => 
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "blazorcrud.api v1");
+    c.DefaultModelsExpandDepth(-1);
+});
 
 app.UseHttpsRedirection();
 app.UseBlazorFrameworkFiles();
