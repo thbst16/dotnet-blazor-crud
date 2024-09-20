10d9585e015102c155d390bacf3adff56ccdcdc3
Thomas Beck <thomas@beckshome.com>
2024-09-19 20:00:00 -0600

    updated cascading validation properties

--- Blazorcrud.Shared/Models/PersonValidator.cs
+++ Blazorcrud.Shared/Models/PersonValidator.cs
@@ -6,7 +6,7 @@ namespace Blazorcrud.Shared.Models
     {
         public PersonValidator()
         {
-            CascadeMode = CascadeMode.Stop;
+            RuleLevelCascadeMode = CascadeMode.Stop;
 
             RuleFor(person => person.FirstName).NotEmpty().WithMessage("First name is a required field.")
                 .Length(3, 50).WithMessage("First name must be between 3 and 50 characters.");
