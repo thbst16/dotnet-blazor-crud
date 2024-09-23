2c6c4f13cb204ab580a5f65ec7af9ceb42e7468a
Thomas Beck <thomas@beckshome.com>
2024-09-22 20:00:00 -0600

    fixed nullable reference in server

--- Blazorcrud.Server/Authorization/AuthenticateResponse.cs
+++ Blazorcrud.Server/Authorization/AuthenticateResponse.cs
@@ -4,8 +4,8 @@ using Blazorcrud.Shared.Models;
 public class AuthenticateResponse
 {
     public int Id {get; set;}
-    public string FirstName {get; set;}
-    public string LastName {get; set;}
-    public string Username {get;set;}    
+    public string FirstName {get; set;} = null!;
+    public string LastName {get; set;} = null!;
+    public string Username {get;set;} = null!;
     public string Token { get; set; } = default!;
 }
