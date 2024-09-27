64d37d5b4f329e3a431ed8aced5180b483192e58
Thomas Beck <thomas@beckshome.com>
2024-09-26 20:00:00 -0600

    clean rebuild with no errors or warnings

--- Blazorcrud.Server/Authorization/AuthorizeAttribute.cs
+++ Blazorcrud.Server/Authorization/AuthorizeAttribute.cs
@@ -15,7 +15,7 @@ public class AuthorizeAttribute : Attribute, IAuthorizationFilter
             return;
 
         // authorization
-        var user = (User)context.HttpContext.Items["User"];
+        var user = (User?)context.HttpContext.Items["User"];
         if (user == null)
             context.Result = new JsonResult(new { message = "Unauthorized" }) { StatusCode = StatusCodes.Status401Unauthorized };
     }
